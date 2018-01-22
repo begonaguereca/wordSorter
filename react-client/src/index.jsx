a6b6f13bdf63e642fe17a0562da101cc16b5f0da
Begona Guereca <begonaguereca@gmail.com>
2018-01-22 15:25:29 -0800

    initial commit

---
+++ react-client/src/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+//import List from './components/List.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  // componentDidMount() {
+  //   $.ajax({
+  //     url: '/items',
+  //     success: (data) => {},
+  //     error: (err) => {
+  //       console.log('err', err);
+  //     }
+  //   });
+  }
+
+  render () {
+    return (
+    <div>
+      <h1>Sort Word</h1>
+    </div>
+  )}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
